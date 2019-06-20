3275d6336fca90a4b1337a86c26abcff36b719b8
asakuracl <champi.ludo@hotmail.fr>
2019-06-19 22:34:57 -0300

    fix syntax to markers l25

--- js/map.js
+++ js/map.js
@@ -22,7 +22,7 @@ class leafMap {
         for (let station of stations) {
             const markers = L.marker([station.position.lat, station.position.lng]).addTo(map);
 
-            markers.addEventListener('click', function () {
+            markers.addEventListener("click", function () {
                 stationName.innerHTML = station.name;
                 stationAddress.innerHTML = station.address;
                 stationStatus.innerHTML = station.status;
