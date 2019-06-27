19014765f17b6dbace761d1b6f1fa9f244e99227
asakuracl <champi.ludo@hotmail.fr>
2019-06-27 12:39:49 -0300

    replace input by button

--- js/reserve.js
+++ js/reserve.js
@@ -1,1 +1,23 @@
-// Reserve
+// Reserve 
+
+class reservation {
+    constructor(btReserve) {
+        this.btReserve = btReserve;
+        this.bike = document.querySelector("#stationBikes");
+        this.canvasHead = document.querySelector("#canvasHead");
+        //
+        this.btReserve.addEventListener("click", this.reserve.bind(this));
+        //
+    }
+
+    //
+    reserve() {
+        this.bike.innerHTML -= 1;
+        this.canvasHead.display = "block";
+    }
+    //
+}
+
+const btReserve = document.querySelector("#formulBouton");
+
+const reserve = new reservation(btReserve);
