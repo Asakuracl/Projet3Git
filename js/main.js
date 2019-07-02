b3c368fa29634df1e1e4ba44e965e6ede13cb09e
asakuracl <champi.ludo@hotmail.fr>
2019-07-02 16:14:08 -0300

    add animation on bt & canvasHead

--- js/main.js
+++ js/main.js
@@ -2,8 +2,16 @@
 
 $(".jqButton").hide();
 
-$("#slideImage").mouseover(function () {
+$("#slideImage").on("mouseover", function () {
     $(".jqButton").animate({
         height: "show"
     }, 800)
+});
+
+//anime canvas pop
+
+$("#formulBouton").on("click", function () {
+    $("#formul").add("#formulBouton").animate({
+        opacity: "hide"
+    }, 1000)
 });
