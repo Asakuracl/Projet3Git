68550098757d7012138a9570b02ee6d1b90f2206
asakuracl <champi.ludo@hotmail.fr>
2019-07-04 22:20:44 -0300

    find a way too animate

--- js/main.js
+++ js/main.js
@@ -9,17 +9,18 @@ $("#slideImage").on("mouseover", function () {
 });
 
 //anime canvas pop
-/*
+
+/* casse le code !
 $("#formulBouton").on("click", function () {
     $("#formul").add("#formulBouton").animate({
         opacity: "hide"
     }, 800)
 });
 */
-
-
+/*
 $("#formulBouton").on("click", function () {
     $("#canvas").animate({
         opacity: "1"
     }, 1000)
 });
+*/
