3d2f2e283f79896c16fd25abfb3f5cf0bceae86c
asakuracl <champi.ludo@hotmail.fr>
2019-07-11 18:45:06 -0300

    creat begin timer, add some test on countdown

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,25 @@
 // timer
+class countdown {
+    constructor(btValid, minNow) {
+        this.btValid = btValid;
+        this.minNow = minNow;
+        console.log(minNow)
+        this.btValid.addEventListener("click", this.start.bind(this));
+
+    }
+
+    start() {
+        this.x = setInterval(function () {
+            this.now = new Date().getTime();
+
+            this.distance =
+                this.minutes = Math.floor
+        })
+    }
+}
+
+const btValid = document.querySelector("#btValid");
+const minNow = new Date().getMinutes();
+
+
+const timer = new countdown(btValid, minNow);
