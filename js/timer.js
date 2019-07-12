5a9f75b812bde1e2fe9d7dc2796695b328f28a64
asakuracl <champi.ludo@hotmail.fr>
2019-07-11 23:20:59 -0300

    work on timer count

--- js/timer.js
+++ js/timer.js
@@ -3,18 +3,26 @@ class countdown {
     constructor(btValid, minNow) {
         this.btValid = btValid;
         this.minNow = minNow;
-        console.log(minNow)
+        this.tempsData = document.querySelector("#tempsData");
         this.btValid.addEventListener("click", this.start.bind(this));
 
     }
 
     start() {
         this.x = setInterval(function () {
-            this.now = new Date().getTime();
+            this.now = new Date().getMinutes() + 20;
+            this.nowTest = new Date().getMinutes()
 
-            this.distance =
-                this.minutes = Math.floor
-        })
+            //this.distance = this.minNow - this.now;
+            this.distance = this.now - this.nowTest;
+            console.log(this.distance)
+
+            this.minutes = Math.floor((this.distance % (1000 * 60 * 60)) / (1000 * 60));
+            console.log(this.minutes)
+            this.seconds = Math.floor((this.distance % (1000 * 60)) / (1000));
+            console.log(this.seconds)
+            this.tempsData.innerHTML = this.minutes + "m" + this.seconds + "s";
+        }, 1000);
     }
 }
 
