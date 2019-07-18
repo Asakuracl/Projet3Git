dedafb68cb20b13d632ca7c851d5626581d677d7
asakuracl <champi.ludo@hotmail.fr>
2019-07-18 15:51:55 -0300

    try add timer on refresh

--- js/timer.js
+++ js/timer.js
@@ -4,13 +4,16 @@ class countdown {
         this.btValid = btValid;
         this.tempsData = document.querySelector("#tempsData");
 
+        //
+        this.textData = document.querySelector("#textData");
+        //
         this.addMin = 20;
-
-        //this.count = null;
 
         this.countDownDate = new Date().getTime() + this.addMin * 60000;
 
         this.btValid.addEventListener("click", this.start.bind(this));
+
+        this.timerOn();
 
     }
 
@@ -29,14 +32,43 @@ class countdown {
 
         this.tempsData.innerHTML = this.minutes + "m" + this.seconds + "s";
 
-        sessionStorage.setItem("timer", this.distance);
+        this.getTimer = sessionStorage.setItem("getTimer", this.distance);
 
         if (this.distance < 0) {
             clearInterval(this.count);
             this.tempsData.innerHTML = "écoulé !"
         }
 
-        //add a if on sessionstorage timer check p3
+
+        let getCount = {
+            heure: this.minutes + "m" + this.seconds + "s"
+        };
+
+        setInterval(getCount, 1000);
+        console.log(getCount.heure)
+
+        let getCount_json = JSON.stringify(getCount);
+        sessionStorage.setItem("objet", getCount_json);
+
+    }
+
+    //add a if on sessionstorage timer check p3
+
+    timerOn() {
+        let getCount_json = sessionStorage.getItem("objet");
+        let getCount = JSON.parse(getCount_json);
+        console.log(getCount);
+
+
+        /*
+                if (sessionStorage.getItem("getTimer") > 0) {
+        
+                    this.textData.style.opacity = "1";
+        
+                    this.tempsData.innerHTML = getCounts.heure;
+        
+                }
+        */
     }
 
     start() {
