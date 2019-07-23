cebafc9fa6d63457e11465b54c0d5a91533152b3
asakuracl <champi.ludo@hotmail.fr>
2019-07-22 23:06:15 -0300

    clean JSON method

--- js/timer.js
+++ js/timer.js
@@ -39,32 +39,20 @@ class countdown {
         this.tempsData.innerHTML = this.minutes + "m" + this.seconds + "s";
 
         this.getTimer = sessionStorage.setItem("getTimer", this.distance);
-        //
+
         this.saveTimer = sessionStorage.setItem("saveTimer", this.countDownDate);
-        //
+
 
         if (this.distance < 0) {
             clearInterval(this.count);
             this.tempsData.innerHTML = "écoulé !"
         }
 
-        /*
-                let getCount = {
-                    heure: this.minutes + "m" + this.seconds + "s"
-                };
-        
-                let getCount_json = JSON.stringify(getCount);
-        
-                sessionStorage.setItem("objet", getCount_json);
-        */
     }
 
     //add a if on sessionstorage timer check p3
 
     timerOn() {
-        //let getCount_json = sessionStorage.getItem("objet");
-        //let getCount = JSON.parse(getCount_json);
-
 
         //sessionStorage.removeItem("objet");
         //sessionStorage.removeItem("getTimer");
@@ -77,14 +65,6 @@ class countdown {
             this.addressData.innerHTML = localStorage.getItem("address");
             this.nomValue.innerHTML = localStorage.getItem("nomValue");
             this.prenomValue.innerHTML = localStorage.getItem("prenomValue");
-
-            /*
-                        this.tempsData.innerHTML = getCount.heure;
-                        console.log(getCount.heure);
-                        let distance = sessionStorage.getItem("getTimer");
-            
-                        this.countDownDate = new Date().getTime() + parseInt(distance, 10);
-            */
 
             this.countDownDate = sessionStorage.getItem("saveTimer");
 
